5fc8988441bb7874eae3372134fab36833b5f4c3
robert.makyla <robert.makyla@gmail.com>
2016-11-13 14:03:35 +0100

    tail rec - Direct Way vs. Continuation Passing style

--- src/main/scala-2.11/fp/aa_TailRec.scala
+++ src/main/scala-2.11/fp/aa_TailRec.scala
@@ -2,7 +2,7 @@ package fp
 
 import scala.annotation.tailrec
 
-object Intro {
+object aa_TailRec {
 
   /**
    * Pure functions :
@@ -63,7 +63,35 @@ object Intro {
    * Pure Functions are like black box.
    */
 
+  /**
+   * factorial(n) = 1 * 2 * 3 * ... * n
+   */
   @tailrec
-  def fib(n: Int, last: Int = 0, cur: Int = 1): Int = if (n == 0) last else fib(n - 1, cur, last + cur)
+  def factorialDirectWay(n: Int, acc: Int = 1): Int = if (n == 0) acc else factorialDirectWay(n - 1, acc * n)
+
+  @tailrec // continuation passing style
+  def factorialCPS(n: Int, i: Int = 1, acc: Int = 1): Int = if (n == 0) acc else factorialCPS(n - 1, i + 1, acc * i)
+
+  /**
+   * sum(n) = 0 + 1 + 2 + 3 + ... + n
+   */
+  @tailrec
+  def sumDirectWay(n: Int, acc: Int = 0): Int = if (n == 0) acc else sumDirectWay(n - 1, acc + n)
+
+  @tailrec // continuation passing style
+  def sumCPS(n: Int, i: Int = 1, acc: Int = 0): Int = if (n == 0) acc else sumCPS(n - 1, i + 1, acc + i)
+
+  /**
+   * fib(n) = fib(n-1) + fib(n-2)
+   */
+  @tailrec // continuation passing style
+  def fibCPS(n: Int, b: Int = 0, a: Int = 1): Int = if (n == 0) a else fibCPS(n - 1, a, a + b)
+
+  /**
+   * mouse(n) = mouse(n-1) * mouse(n-3) + 1
+   */
+  @tailrec // continuation passing style
+  def mouseCPS(n: Int, c: Int = 1, b: Int = 1, a: Int = 1): Int = if (n <= 2) a else mouseCPS(n - 1, b, a, a * c + 1)
+
 
 }
---
+++ src/test/scala-2.11/fp/aa_TailRecSpec.scala
@@ -0,0 +1,62 @@
+package fp
+
+import org.scalatest.{FreeSpec, MustMatchers}
+import fp.aa_TailRec._
+
+class aa_TailRecSpec extends FreeSpec with MustMatchers{
+
+  "Factorial" - {
+    "direct way" in {
+      factorialDirectWay(1) mustBe 1
+      factorialDirectWay(2) mustBe 2
+      factorialDirectWay(3) mustBe 6
+      factorialDirectWay(4) mustBe 24
+    }
+
+    "continuation passing style" in {
+      factorialCPS(1) mustBe 1
+      factorialCPS(2) mustBe 2
+      factorialCPS(3) mustBe 6
+      factorialCPS(4) mustBe 24
+    }
+  }
+
+  "Sum" - {
+    "direct way" in {
+      sumDirectWay(0) mustBe 0
+      sumDirectWay(1) mustBe 1
+      sumDirectWay(2) mustBe 3
+      sumDirectWay(3) mustBe 6
+      sumDirectWay(4) mustBe 10
+    }
+    "continuation passing style" in {
+      sumCPS(0) mustBe 0
+      sumCPS(1) mustBe 1
+      sumCPS(2) mustBe 3
+      sumCPS(3) mustBe 6
+      sumCPS(4) mustBe 10
+    }
+  }
+
+  "Fibonacci number - continuation passing style" in {
+    fibCPS(0) mustBe 1
+    fibCPS(1) mustBe 1
+    fibCPS(2) mustBe 2
+    fibCPS(3) mustBe 3
+    fibCPS(4) mustBe 5
+    fibCPS(5) mustBe 8
+    fibCPS(6) mustBe 13
+  }
+
+  "Mouse - continuation passing style" in {
+    mouseCPS(0) mustBe 1
+    mouseCPS(1) mustBe 1
+    mouseCPS(2) mustBe 1
+    mouseCPS(3) mustBe 2
+    mouseCPS(4) mustBe 3
+    mouseCPS(5) mustBe 4
+    mouseCPS(6) mustBe 9
+    mouseCPS(7) mustBe 28
+  }
+
+}
