cdd56023d0c8d795f920a0ca50e085b010ccccea
robert.makyla <robert.makyla@gmail.com>
2017-08-24 22:24:52 +0200

    local mutable state and contextual purity

--- src/main/scala-2.12/fp/ah_IOMonad.scala
+++ src/main/scala-2.12/fp/ah_IOMonad.scala
@@ -71,4 +71,43 @@ object ah_IOMonad {
    * - writing efficient, streaming I/O will generally involve monolithic loops (ex processing file 1 line at time)
    *   (if we don't want to load whole file to memory - streaming is the answer, but processing loops are ugly - we cannot compose them)
    */
+
+  /** Local efects and mutable state
+   *
+   * Function is pure if it makes no side effects and if it's referentially transparent
+   * (same args => always the same result)
+   *
+   * But it can be still referentially transparent with local effects, and mutable state (only in local scope)
+   *
+   * eg. come loops with var and changing it's value each iteration - it happens under Scala known functions
+   */
+
+  /**
+   * Purity is contextual
+   *
+   * ------------- eg 1: (memory management)
+   *
+   * Every construction of data (instantiating) has a side effect in Scala.
+   *
+   *   case class Foo(msg: String)
+   *   val x = Foo("hello") // creating unique object in memory and returning reference to this new object.
+   *
+   * But most Scala programs don't use references, just values (using ==, not eq/equals to compare them)
+   * So in such context it's pure..
+   *
+   * We could track where memory is allocated / what references are created..
+   * but since we have automatic memory management we don't want that - it's not interesting for us.
+   * IT'S NOT THE CONTEXT IN WHICH WE WANT TO BE PURE !
+   *
+   * ------------- eg 2: (logging)
+   *
+   * The log.info("") / println("") can be called as side effect but we need to answer ourselves, what kind of side effect matters?
+   * What kind of side effects can influence the way the application works ?
+   * logging shouldn't so in our context it's also pure..
+   *
+   * Most of functions have hidden dependencies on the IO subsystem (logger/System.out.println)
+   * or by creating objects in memory and GC'ing them.
+   *
+   * Making these side effects (like logging, or creating objects) is a choice we make as programmers !
+   */
 }
